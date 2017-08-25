ccab80accd8f3a9e6e6d88067bd7d895db6b6522
Ying Jin <jin914@usc.edu>
2017-08-25 13:17:54 -0700

    trying hard

---
+++ homework-resources/hw1/therani.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if(argc < 3){
+    cerr << "Please provide an input and output file." << endl;
+    return 1;
+  }
+  ifstream input(argv[1]);
+  ofstream output(argv[2]);
+
+  int experiments;
+  int *numsubjects;
+  string **history;
+  string curr;
+
+  //for each experiment, you will need to keep track of the number of subjects; and for each subject, of their history
+
+  //loop to read The Rani's logbook.
+  while(getline(input, curr)) {
+	  stringstream ss;
+	  ss << curr;
+	  ss >> curr;
+	  if (curr == "START") {
+	    // Hint: This does not cover all errors yet.
+		  int n;
+		  ss >> n;
+		  if (ss.fail()) {
+		    output << "Error - incorrect command: " << curr << endl;
+  		    output << "Parameter should be an integer" << endl;
+		  }
+		  else if (n < 0) {
+		    output << "Error - incorrect command: " << curr << endl;
+		    output << "Number out of range" << endl;
+		  }
+		  else {
+			  
+		  }
+	  }
+	  else if (curr == "ADD") {
+
+	  }
+	  else if (curr == "MOVE") {
+
+	  }
+	  else if (curr == "QUERY") {
+
+	  }
+	  else {
+		  
+	  }
+  }
+  return 0;
+}
